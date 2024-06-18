8c5801f9836b2601a2f38ad1f193cc8a629c8969
Diego DPL <info@diegodpl.com>
2024-06-18 18:04:36 +0200

    home css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Home.css';
 import { Container } from '@mui/material';
+import { motion } from "framer-motion"
 
 function Home() {
 
   return (
 
-    <Container component="main" sx={{ flexGrow: 1, mt: 4 }}> 
-      <div className='home'>
+    <Container component="main" sx={{ flexGrow: 1, mt: 4 , width: '100%'}}> 
+      <motion.div className='home'
+       initial={{ opacity: 0, scale: 0.5 }}
+       animate={{ opacity: 1, scale: 1 }}
+       transition={{ duration: 0.8 }}
+      >
         <div className='columUno'>
           <h1 className='tituloHome'>Tu bienestar emocional es nuestra prioridad</h1>
           <h2 className='descripcionHome'>Ofrecemos servicios de psicología personalizados. Nuestro equipo te ayuda a superar desafíos emocionales y mejorar tu bienestar mental.</h2>
@@ -20,13 +25,17 @@ function Home() {
           <img className='logoTransparente' src={process.env.PUBLIC_URL + 'assets/logo-transparente.png'} alt="Logo" />
           <div className='circle'></div>
         </div>
-      </div>
-      <div className='scrolServicios'> 
+      </motion.div>
+      <motion.div className='scrolServicios'
+             initial={{ opacity: 0 , scale: 1}}
+             whileInView={{ opacity: 1, scale: 1}}
+             transition={{ duration: 0.8 }}
+      > 
         <h1 className='tituloScroll1'>Somos especialistas en:</h1>
         <h2 className='subtituloScroll1'>Trastornos de la Personalidad. </h2>
         <h2 className='subtituloScroll1'>Psicologia perinatal. </h2>
         <h2 className='subtituloScroll1'>Trastornos de la Conducta alimentaria. </h2>
-      </div>
+      </motion.div>
     </Container> 
 
   );
