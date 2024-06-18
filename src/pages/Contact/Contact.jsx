db2d809a749b625968f567784556c8cce978625c
fgrinan <fgrinan@trustportal.org>
2024-06-18 12:32:25 +0200

    contact page revamped

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,33 +1,138 @@
-import React from 'react';
-import './Contact.css';
-import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import Button from '@mui/material/Button';
+import React, { useState } from "react";
+import "./Contact.css";
+import { TextField, Button, MenuItem, IconButton, Grid } from "@mui/material";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
-function Contact({servicios}) {
-    return (
-        <div className="contactContainer">
-            <h2 className='contactTitle'>Contacta conmigo</h2>
-            <form className="contactForm">
-                <TextField required className="nombreContact" id="nombre" label="Nombre" defaultValue=""/>
-                <TextField required className="emailContact" id="email" label="Email" defaultValue=""/>
-                
-                <div>
-                    <InputLabel id="servicios">Servicios</InputLabel>
-                    <Select className="servicios" labelId="servicios" id="servicios" value="Servicios" label="Servicios">
-                        {servicios.map((servicio) => (
-                            <MenuItem className='setvicioItem' key={servicio.Servicio} value={servicio.Servicio}>{servicio.Servicio}</MenuItem>
-                        ))}
-                    </Select>
-                </div>
+function Contact({ servicios }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [service, setService] = useState("");
+  const [message, setMessage] = useState("");
 
-            <TextField className="texAreaContacto" id="texAreaContacto" label="Cuentanos" multiline rows={5} defaultValue="Escribe aqui"/>
-            <Button variant="contained">Contained</Button>
-            </form>
-        </div>
-    )
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({ name, email, service, message });
+  };
+
+  const handleWhatsAppClick = () => {
+    const phoneNumber = "648817588"; // Reemplaza con el número de violeta
+    const url = `https://wa.me/${phoneNumber}`;
+    window.open(url, "_blank");
+  };
+
+  const textFieldStyles = {
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "#408494",
+      },
+      "&:hover fieldset": {
+        borderColor: "#408494",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "#408494",
+      },
+      "& .MuiOutlinedInput-input": {
+        color: "white",
+      },
+    },
+  };
+
+  return (
+    <div className="contactContainer">
+      <h4 className="contact-title">Contacta conmigo</h4>
+      <form onSubmit={handleSubmit} className="contact-formulario">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label="Nombre"
+              fullWidth
+              margin="normal"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              sx={textFieldStyles}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            {/* Puede dejarse vacío o añadir cualquier otro campo o elemento */}
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label="Correo Electrónico"
+              type="email"
+              fullWidth
+              margin="normal"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              sx={textFieldStyles}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              select
+              label="Servicio"
+              fullWidth
+              margin="normal"
+              value={service}
+              onChange={(e) => setService(e.target.value)}
+              required
+              sx={textFieldStyles}
+            >
+              {servicios.map((servicio) => (
+                <MenuItem key={servicio.Servicio} value={servicio.Servicio}>
+                  {servicio.Servicio}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Mensaje"
+              multiline
+              rows={4}
+              fullWidth
+              margin="normal"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              sx={textFieldStyles}
+            />
+          </Grid>
+          <Grid item xs={12} sm={8} style={{ textAlign: "center" }}>
+            <div className="contact-whatssap">
+              <p className="contact-mensaje-whatssap">
+                Si lo deseas, ponte en contacto conmigo vía whatssap!
+              </p>
+              <IconButton
+                aria-label="WhatsApp"
+                //style={{ marginTop: "2rem" }}
+                onClick={handleWhatsAppClick}
+              >
+                <WhatsAppIcon style={{ fontSize: 40, color: "#22c298" }} />
+              </IconButton>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              style={{
+                marginTop: "1rem",
+                backgroundColor: "#E93A7D",
+                borderRadius: "40px",
+                paddingBottom: "10px",
+                paddingTop: "10px",
+              }}
+            >
+              Enviar mensaje
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
