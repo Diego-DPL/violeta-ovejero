4925741fd40020004da17fb9e178859c4ed0bd14
Diego DPL <info@diegodpl.com>
2024-07-16 12:57:55 +0200

    ajuste de calendario estetica

--- src/pages/Reservas/Reservas.jsx
+++ src/pages/Reservas/Reservas.jsx
@@ -6,7 +6,7 @@ import startOfWeek from 'date-fns/startOfWeek';
 import getDay from 'date-fns/getDay';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { es } from 'date-fns/locale';
-import { useNavigate } from 'react-router-dom'; // Asegúrate de que estás usando React Router
+import { useNavigate } from 'react-router-dom';
 
 const locales = {
   'es': es,
@@ -25,8 +25,8 @@ const calendarId = 'dpl.official.dj@gmail.com';
 
 const Reservas = () => {
   const [events, setEvents] = useState([]);
-  const [view, setView] = useState('week'); // Estado para la vista actual
-  const navigate = useNavigate(); // Hook de React Router para redireccionar
+  const [view, setView] = useState('week');
+  const navigate = useNavigate();
 
   useEffect(() => {
     listUpcomingEvents();
@@ -41,14 +41,12 @@ const Reservas = () => {
           start: new Date(item.start.dateTime || item.start.date),
           end: new Date(item.end.dateTime || item.end.date),
           allDay: !item.start.dateTime,
-          color: 'red',
+          color: '#FCFFE3',
           isGoogleEvent: true,
         }));
 
-        // Añadir slots específicos
         const customSlots = generateCustomSlots();
 
-        // Filtrar los slots que se superponen con los eventos de Google
         const availableSlots = customSlots.filter(slot => {
           return !googleEvents.some(event => 
             (slot.start < event.end && slot.end > event.start)
@@ -62,58 +60,64 @@ const Reservas = () => {
   const generateCustomSlots = () => {
     const slots = [];
     const now = new Date();
-    const startDate = new Date(now.getTime() + 24 * 60 * 60 * 1000); // Añadir 24 horas a la fecha actual
-    startDate.setHours(0, 0, 0, 0); // Ajustar a las 00:00 horas
+    const startDate = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+    startDate.setHours(0, 0, 0, 0);
 
-    for (let day = 0; day < 5; day++) { // De lunes a viernes
+    for (let day = 0; day < 5; day++) {
       // Generar slots de 9 AM a 2 PM
       let hour = 9;
-      while (hour < 14) {
+      while (hour < 13) {
         const startSlot = new Date(startDate);
         startSlot.setDate(startDate.getDate() + day);
-        startSlot.setHours(hour, hour % 1 * 60, 0, 0);
+        startSlot.setHours(Math.floor(hour), (hour % 1) * 60, 0, 0);
 
         const endSlot = new Date(startSlot);
         endSlot.setHours(startSlot.getHours() + 1, startSlot.getMinutes());
 
-        if (startSlot > now) { // Solo añadir slots después de las próximas 24 horas
+        if (startSlot > now) {
           slots.push({
             title: 'Disponible',
             start: startSlot,
             end: endSlot,
             allDay: false,
-            color: 'green',
+            color: '#106579',
             isGoogleEvent: false,
           });
         }
 
-        // Añadir 5 minutos de intervalo
-        hour += 1.0833; // 1 hora y 5 minutos (1 + 5/60)
+        hour += 1 + 5 / 60; // 1 hora y 5 minutos
       }
 
       // Generar slots de 4 PM a 9 PM
       hour = 16;
-      while (hour < 21) {
+      var firstSlot = true;
+
+      while (hour < 20) {
         const startSlot = new Date(startDate);
         startSlot.setDate(startDate.getDate() + day);
-        startSlot.setHours(hour, hour % 1 * 60, 0, 0);
+        startSlot.setHours(Math.floor(hour), (hour % 1) * 60, 0, 0);
 
         const endSlot = new Date(startSlot);
         endSlot.setHours(startSlot.getHours() + 1, startSlot.getMinutes());
 
-        if (startSlot > now) { // Solo añadir slots después de las próximas 24 horas
+        if (startSlot > now) {
           slots.push({
             title: 'Disponible',
             start: startSlot,
             end: endSlot,
             allDay: false,
-            color: 'green',
+            color: '#106579',
             isGoogleEvent: false,
           });
         }
 
-        // Añadir 5 minutos de intervalo
-        hour += 1.0833; // 1 hora y 5 minutos (1 + 5/60)
+        if (firstSlot) {
+          hour += 1 + 5.1 / 60; // 1 hora y 5 minutos
+          firstSlot = false;
+        } else {
+          hour += 1 + 5 / 60; // 1 hora y 5 minutos
+        }
+        
       }
     }
 
@@ -121,26 +125,27 @@ const Reservas = () => {
   };
 
   const handleSelectSlot = ({ start, end }) => {
-    const title = 'Reserva de Sesión'; // Título fijo para el evento de reserva
-    // Redirigir a la página de pago y pasar los datos del evento
-    navigate(`/pago?start=${start.toISOString()}&end=${end.toISOString()}&title=${encodeURIComponent(title)}`);
+    // No hacer nada cuando se selecciona un espacio fuera de los slots disponibles
   };
 
   const handleSelectEvent = (event) => {
     if (!event.isGoogleEvent) {
-      // Redirigir a la página de pago y pasar los datos del evento
       navigate(`/pago?start=${event.start.toISOString()}&end=${event.end.toISOString()}&title=${encodeURIComponent(event.title)}`);
     }
   };
 
   const eventPropGetter = (event) => {
     const backgroundColor = event.color;
-    return { style: { backgroundColor } };
+    const style = { backgroundColor };
+    if (event.isGoogleEvent) {
+      style.color = 'black'; // Título del evento en negro
+    }
+    return { style };
   };
 
   return (
-    <div className="flex flex-col mt-[5%] mb-[5%] mr-[7%] ml-[7%]">
-      <h1 className="text-2xl font-bold mb-4">Reservas</h1>
+    <div className="flex flex-col mt-[3%] mb-[5%] mr-[7%] ml-[7%]">
+      <h1 className="text-4xl font-bold text-bg1 mb-4">Reserva tu cita</h1>
       <Calendar
         localizer={localizer}
         events={events}
@@ -150,12 +155,11 @@ const Reservas = () => {
         endAccessor="end"
         min={new Date(0, 0, 0, 9, 0, 0)}
         max={new Date(0, 0, 0, 21, 0, 0)}
-        style={{ height: 500 }}
         selectable
         onSelectSlot={handleSelectSlot}
-        onSelectEvent={handleSelectEvent} // Manejador para cuando se selecciona un evento
-        onView={(newView) => setView(newView)} // Manejador para cambiar la vista
-        eventPropGetter={eventPropGetter} // Establecer el color de fondo de los eventos
+        onSelectEvent={handleSelectEvent}
+        onView={(newView) => setView(newView)}
+        eventPropGetter={eventPropGetter}
       />
     </div>
   );
